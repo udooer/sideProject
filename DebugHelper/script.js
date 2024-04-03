117604f14f5b2722a3099c01f6d5230919452284
shane_hsu <hunghsuyong114shane@gmail.com>
2024-04-03 00:44:27 -0700

    feat:
    Add multiple keywords  highlighting logic

--- DebugHelper/script.js
+++ DebugHelper/script.js
@@ -1,51 +1,74 @@
 function highlightKeyword() {
-  var keyword = document.getElementById("keywordInput").value;
-  var fileInput = document.getElementById("fileInput");
-  var contentDiv = document.getElementById("content");
+    const keywordInputs = document.getElementsByClassName("keywordInput");
+    const colorSelects = document.getElementsByClassName("colorSelect");
+    const fileInput = document.getElementById("fileInput");
+    const contentDiv = document.getElementById("content");
 
-  if (fileInput.files.length === 0) {
-    alert("Please select a file.");
-    return;
-  }
-
-  var file = fileInput.files[0];
-  var reader = new FileReader();
-
-  reader.onload = function (e) {
-    var lines = e.target.result.split("\n");
-    contentDiv.innerHTML = "";
-
-    for (var i = 0; i < lines.length; i++) {
-      if (lines[i].includes(keyword)) {
-        contentDiv.innerHTML +=
-          '<span class="highlight">' + lines[i] + "</span><br>";
-      } else {
-        contentDiv.innerHTML += lines[i] + "<br>";
-      }
+    if (fileInput.files.length === 0) {
+        alert("Please select a file.");
+        return;
     }
-  };
 
-  reader.readAsText(file);
+    let isContainKeyWord = false;
+    for (var i = keywordInputs.length - 1; i > -1; i--) {
+        if (keywordInputs[i].value != "") {
+            isContainKeyWord = true;
+        }
+        else {
+            keywordInputs[i].remove()
+            colorSelects[i].remove()
+        }
+    }
+    if (!isContainKeyWord) {
+        alert("Please input keyword");
+        return;
+    }
+
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        var lines = e.target.result.split("\n");
+        contentDiv.innerHTML = "";
+
+        for (var i = 0; i < lines.length; i++) {
+            let hasHighlighted = false;
+            for (var j = 0; j < keywordInputs.length; j++) {
+                if (lines[i].includes(keywordInputs[j].value)) {
+                    contentDiv.innerHTML +=
+                        '<span style="color:' + colorSelects[j].value + '">' + lines[i] + "</span><br>";
+                    hasHighlighted = true;
+                    break;
+                }
+            }
+            if (!hasHighlighted) {
+                contentDiv.innerHTML += lines[i] + "<br>";
+            }
+
+        }
+    };
+
+    reader.readAsText(file);
 }
 
 function addKeywordColorPair() {
-  const keywordColorPairs = document.getElementById("keywordColorPairs");
+    const keywordColorPairs = document.getElementById("keywordColorPairs");
 
-  const keywordInput = document.createElement("input");
-  keywordInput.type = "text";
-  keywordInput.className = "keywordInput";
-  keywordInput.placeholder = "Enter keyword";
+    const keywordInput = document.createElement("input");
+    keywordInput.type = "text";
+    keywordInput.className = "keywordInput";
+    keywordInput.placeholder = "Enter keyword";
 
-  const colorSelect = document.createElement("select");
-  colorSelect.className = "colorSelect";
-  ["Red", "Blue", "Green"].forEach((color) => {
-    let colorOption = document.createElement("option");
-    colorOption.value = color.toLowerCase();
-    colorOption.textContent = color;
-    colorSelect.append(colorOption);
-  });
+    const colorSelect = document.createElement("select");
+    colorSelect.className = "colorSelect";
+    ["Red", "Blue", "Green"].forEach((color) => {
+        const colorOption = document.createElement("option");
+        colorOption.value = color.toLowerCase();
+        colorOption.textContent = color;
+        colorSelect.append(colorOption);
+    });
 
-  keywordColorPairs.append(keywordInput);
-  keywordColorPairs.append(colorSelect);
-  keywordColorPairs.append(document.createElement("br"));
+    keywordColorPairs.append(keywordInput);
+    keywordColorPairs.append(colorSelect);
+    keywordColorPairs.append(document.createElement("br"));
 }
